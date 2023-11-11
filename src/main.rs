25b7fe630d6ae0cd15de4e52955f411052ffacca
lingllqs <lingllqs@163.com>
2023-11-11 22:33:43 +0800

    add test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std] // 金庸标准库
 #![no_main] // 不使用一般入口点
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
@@ -11,11 +14,46 @@ fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
-
 /// no_mangle 属性防止编译器修改函数名
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello Rust{}", "!");
-    panic!("Something wrong!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
